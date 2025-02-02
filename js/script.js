0e1c3b140baa5781c98118640b131295791ec26d
Aron Singel <108942904+Shrekcoding@users.noreply.github.com>
2025-02-02 15:55:18 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let menus = document.querySelector("nav");
+let menuBtn = document.querySelector(".menu-btn");
+let closeBTN = document.querySelector(".close-btn");
+
+
+menuBtn.addEventListener("click", function () {
+    menus.classList.add("active");
+})
+
+closeBTN.addEventListener("click", function () {
+    menus.classList.remove("active");
+})
